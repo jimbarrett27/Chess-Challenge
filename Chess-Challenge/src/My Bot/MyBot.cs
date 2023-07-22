5d66d8096d276e7c7a967df067035f7828281940
Jim Barrett <jimbarrett27@gmail.com>
2023-07-22 12:48:43 +0200

    start writing

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,22 @@
 ﻿using ChessChallenge.API;
+using System.Collections.Generic;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+
+        Random rand = new Random();
+
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        List<Move> pawnMoves = new List<Move>();
+        foreach (Move move in moves) {
+            if (move.MovePieceType == PieceType.Pawn) pawnMoves.Add(move);
+        }
+
+        if (pawnMoves.Count > 0) return pawnMoves[rand.Next(pawnMoves.Count)];
+        
+        return moves[rand.Next(moves.Length)];
     }
 }
